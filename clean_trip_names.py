e65cf2c68210d68a4904f7f775a51bb28af161a8
Joel Carranza <joel.carranza@gmail.com>
2012-09-07 08:36:29 -0500

    change trip in GTFS to be a function of route+direction

--- clean_trip_names.py
+++ clean_trip_names.py
@@ -10,12 +10,33 @@ def normalize(name):
   name = re.sub(r'\s+',' ',name)
   return name
 
-def process(trips):
+def process(routes,trips):
   csvio.transform(trips,'trip_headsign',normalize)
+
+  # encode the route and direction into the trip-id. 
+  # OTP returns the trip_id in the plan results, and this lets us get access
+  # to the route and the direction which we would otherwise not be able to do
+
+  # the "route" here i refer to is really the code/number - which we've ensured
+  # already is in route.route_short_name
+  route_code_by_id = {}
+  for r in routes:
+    route_code_by_id[r['route_id']] = int(r['route_short_name'])
+  sequences = dict()
+  for trip in trips:
+    route_code = route_code_by_id[trip['route_id']]
+    key = "%s-%s" % (route_code,trip['direction_id'])
+    if key in sequences:
+      n = sequences[key]+1
+    else:
+      n = 1
+    trip['trip_id'] = "%s-%s" % (key,n)
+    sequences[key] = n
   return trips
 
 if __name__ == '__main__':
   datadir = sys.argv[1]
+  routes = csvio.read('%s/routes.txt' % datadir)
   trips = csvio.read('%s/trips.txt' % datadir)
-  trips = process(trips)
+  trips = process(routes,trips)
   csvio.write('%s/trips.txt' % datadir,trips)
